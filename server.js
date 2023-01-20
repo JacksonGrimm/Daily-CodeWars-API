b051417509954fbaf26226d498f1538289a49234
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-20 14:06:38 -0700

    moved cors

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ let lastDate = "";
 app.use(checkNewDay);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
 dotenv.config();
 
 app.listen(PORT, () => {
